886a692f3a4db325e6e9f22954e62a564e9e9002
Vitaly <57191500+vkgv@users.noreply.github.com>
2019-10-31 11:45:48 +0300

    New project2

--- Klasswork1/Program.cs
+++ Klasswork1/Program.cs
@@ -5,14 +5,14 @@ namespace Klasswork1
 {
     internal class Program
     {
-        private static void Main()
+        static void Main()
         {
             Console.WriteLine("Enter your name");
             string name = Console.ReadLine();
             Thread.Sleep(5000);
             Console.WriteLine("Hello, " +name);
             Thread.Sleep(5000);
-            Console.WriteLine("See you soon, " + name);
+            Console.WriteLine("See you soon, " +name);
             Console.ReadKey();
         }
     }

8da405bf82a7c352bda40f6b8244c9f934669f04
Vitaly <57191500+vkgv@users.noreply.github.com>
2019-10-31 11:20:18 +0300

    New project

---
+++ Klasswork1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Klasswork1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine("Hello, " +name);
+            Thread.Sleep(5000);
+            Console.WriteLine("See you soon, " + name);
+            Console.ReadKey();
+        }
+    }
+}

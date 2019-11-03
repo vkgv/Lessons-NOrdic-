7b89b859b772bca531e2315792a0a24ed9d4c458
Vitaly <57191500+vkgv@users.noreply.github.com>
2019-11-03 18:51:37 +0300

    HomeWork of the 2 lesson added

---
+++ 02.Homework/Lesson2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            string S1 = Console.ReadLine();
+            int a = int.Parse(S1);
+            Console.WriteLine("Введите второе число");
+            string S2 = Console.ReadLine();
+            int b = int.Parse(S2);
+            Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+            Console.WriteLine("{0}+{1}={2}", a, b, a - b);
+            Console.WriteLine("{0}+{1}={2}", a, b, a * b);
+            Console.ReadKey();
+        }
+    }
+}
